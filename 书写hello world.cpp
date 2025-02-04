b0b8520a48bf4e121fbae23ef2310d59bc41284a
Elaina0510 <101503167+Elaina0510@users.noreply.github.com>
2025-02-05 00:19:40 +0800

    Add files via upload

---
+++ 书写hello world.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+
+#define LJH 666
+int main()
+{
+	cout << "我真的" << LJH << endl;
+	//输出大帅哥，这TM叫单行注释
+
+	/*看到没有，这TM就叫多行注释
+	我是大帅哥
+	哈哈哈哈哈哈*/
+
+	//变量创建的语法：数据类型 变量名 = 变量初始值
+	int     a   = 555;
+
+	cout << "a是" << a << endl;
+
+	/*常量的定义方式
+	1.#define宏常量
+	2.const修饰的变量*/
+	cout << "我是；" << LJH << endl;
+	
+	system("pause");
+
+	     return 0;
+
+}
+
+
+
+	
+ 

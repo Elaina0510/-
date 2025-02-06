9dd32a42cfbeb802ab73bd266bde9198caa077b0
Elaina0510 <101503167+Elaina0510@users.noreply.github.com>
2025-02-06 16:12:07 +0800

    数据类型

---
+++ 05转义字符.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main5() {
+
+	//转义字符
+	//1.换行符 \n
+	cout << "hello world" << endl;
+	cout << "hello world\n";
+
+
+    //2.反斜杠 \\
+	
+	cout << "\\"<<endl ;
+
+	//3.水平制表符 \t(占八个空格）
+
+	cout << "aaa\t哇我好靓仔啊" << endl;
+	cout << "66\t哇我好靓仔啊" << endl;
+	cout << "948483\t哇我好靓仔啊" << endl;
+
+
+	system("pause");
+
+	return 0;
+}

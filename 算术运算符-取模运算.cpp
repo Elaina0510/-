948049a0df752b0ff20144ad332a7c2582b51e57
Elaina0510 <101503167+Elaina0510@users.noreply.github.com>
2025-02-08 15:23:09 +0800

    运算符

---
+++ 算术运算符-取模运算.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+	int a1 = 10;
+	int b1 = 3;
+	cout << a1 % b1 << endl;
+
+	int a2 = 10;
+	int b2 = 0;
+	//cout << a2 % b2 << endl;
+	//除数不可为0， 所以也做不了取模运算
+
+
+
+	//两个小数不可以做取模运算
+
+
+
+	system("pause");
+	
+	return 0;
+
+}

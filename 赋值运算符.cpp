4b4329cae8c2b9c462b5c99cdf9cec547ce96013
Elaina0510 <101503167+Elaina0510@users.noreply.github.com>
2025-02-11 22:27:15 +0800

    运算符

---
+++ 赋值运算符.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+	//¸³ÖµÔËËã·û
+
+	// =
+	int a = 10;
+	a = 16;
+	cout << "a=" << a << endl;
+
+	// +=
+	a += 2;  // a = a + 2
+	cout << "a=" << a << endl;
+
+	// -=
+	a = 30;
+	a -= 2; // a = a - 30
+	cout << "a=" << a << endl;
+
+	// *=
+	a *= 2;
+	cout << "a=" << a << endl;
+
+	// /=
+	a /= 3;
+	cout << "a=" << a << endl;
+
+	// %=
+	a %= 2; // a= a % 2
+	cout << "a=" << a << endl;
+
+
+
+	system("pause");
+
+	return 0;
+}
